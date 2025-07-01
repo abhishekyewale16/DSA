c6606e76cc7b6d0ce4acce774d3ae882bb55e3ff
abhishekyewale16 <abhishekyewale038@gmail.com>
2025-07-01 16:19:23 +0530

    Added a Genarte Random Numbers program

---
+++ genraterandomnumbres.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class genraterandomnumbres{
+
+    public static void main(String[] args) {
+        
+        // Approch 1
+        /* 
+        Random rand=new Random();
+
+        int rand_int=rand.nextInt(999);
+
+        System.out.println("The Rand Number is :"+ rand_int);
+
+        double rand_dbl = rand.nextDouble() * 1000;
+        System.out.println("The Rand Double is :"+ rand_dbl);
+        */
+
+        // Approcah 2
+        System.out.println(Math.random());
+
+    }  
+}
