eae9d4073ba36925183bb86de096f4be4834c61a
abhishekyewale16 <abhishekyewale038@gmail.com>
2025-08-19 16:26:32 +0530

    Fix logic to correctly find the minimum number in the array

--- MinMaxElementsArray.java
+++ MinMaxElementsArray.java
@@ -1,13 +1,23 @@
 public class MinMaxElementsArray {
+
     public static void main(String[] args) {
+        
         int a[]={50,60,25,46,47,1};
         int max=a[0];
-
-        for(int i=1; i<a.length; i++){
-            if(a[i] > max){  // Change < to >
+        int min=a[0];
+        for(int i=1;i<a.length;i++){
+            if(a[i]>max){
                 max=a[i];
             }
         }
-        System.out.println("The Maximum Number in Array is:" +max); 
+        System.out.println("The Maximum Number in Array is:"+max);
+
+        for(int i=1; i<a.length; i++){
+            if(a[i]<min){
+                min=a[i];
+            }
+        }
+        System.out.println("The Minimum Number in Array is:"+min);
+    
     }
 }
